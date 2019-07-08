a31dffd83a6cdfdd20be27b7c3b63352d07a6570
kimjakek <kimjakek@sfu.ca>
2019-07-08 14:47:07 -0700

    finished stats_display

--- stats.c
+++ stats.c
@@ -3,7 +3,6 @@
 #include "stats.h"
 
 typedef struct {
-  int facNum; //factory number
   int made; //number of candies the factory reported making
   int eaten; //number of candies kids consumed
   float minDelay; //min time between production and consumtion (milliseconds)
@@ -17,7 +16,6 @@ void stats_init(int num_producers){
 
   //initialize each candy factory
   for (int i = 0; i < num_producers; i++){
-    factories[i].facNum = i;
     factories[i].made = 0;
     factories[i].eaten = 0;
     factories[i].minDelay = 0;
@@ -53,5 +51,24 @@ void stats_record_consumed(int factory_number, double delay_in_ms){
 }
 
 void stats_display(void){
-  
+  printf("Statistics:\n");
+  printf("%8s%10s%10s%10s%10s%10s\n", "Factory#", "#Made", "#Eaten", "Min Delay[ms]", "Avg Delay[ms]", "Max Delay[ms]");
+
+  //finding number of factories in factories array
+  int len = sizeof(factories)/sizeof(factory_t);
+
+  for (int i = 0; i < len; i++){
+    //If the #Made and #Eaten columns don’t match, print an error
+    if(factories[i].made != factories[i].eaten){
+      printf("ERROR: Mismatch between number made and eaten");
+      continue;
+    }
+    printf("%8d%10.5f%10.5f%10.5f%10.5f%10.5f\n", i,
+                                                  factories[i].made,
+                                                  factories[i].eaten,
+                                                  factories[i].minDelay,
+                                                  //find avg by dividing totaldelay by number eaten
+                                                  factories[i].totalDelay/factories[i].eaten,
+                                                  factories[i].maxDelay);
+  }
 }
